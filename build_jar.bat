b53b7681a060378727e2e67831252b132926d039
unknown <dr.denisv2016@yandex.ru>
2018-03-26 16:34:06 +0300

    fix build_jar script

--- build_jar.bat
+++ build_jar.bat
@@ -1,5 +1,5 @@
 cd java
-javac -d .\.. -cp .\..\artifacts\JarImplementorTest.jar -Xlint .\ru\ifmo\rain\vaksman\implementor\Implementor.java .\ru\ifmo\rain\vaksman\implementor\CodeGenerator.java
+javac -d .. -cp ..\artifacts\JarImplementorTest.jar -Xlint ru\ifmo\rain\vaksman\implementor\Implementor.java ru\ifmo\rain\vaksman\implementor\CodeGenerator.java ru\ifmo\rain\vaksman\implementor\UnicodeWriter.java
 cd ..
 jar xf artifacts\JarImplementorTest.jar info\kgeorgiy\java\advanced\implementor\Impler.class info\kgeorgiy\java\advanced\implementor\JarImpler.class info\kgeorgiy\java\advanced\implementor\ImplerException.class
 jar cvfm Implementor.jar manifest.mf ru\ifmo\rain\vaksman\implementor\*.class info\kgeorgiy\java\advanced\implementor\*.class
