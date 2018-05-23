acae26a564ee95bfe3bf6e8eb1c183ea56d2c94b
unknown <dr.denisv2016@yandex.ru>
2018-05-23 03:31:43 +0300

    added limitation per host

--- java/ru/ifmo/rain/vaksman/crawler/WebCrawler.java
+++ java/ru/ifmo/rain/vaksman/crawler/WebCrawler.java
@@ -6,19 +6,55 @@ import info.kgeorgiy.java.advanced.crawler.Downloader;
 import info.kgeorgiy.java.advanced.crawler.Result;
 
 import java.io.IOException;
+import java.net.MalformedURLException;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
+import java.util.Queue;
 import java.util.concurrent.*;
+
+import static info.kgeorgiy.java.advanced.crawler.URLUtils.getHost;
 
 public class WebCrawler implements Crawler {
     private final Downloader downloader;
     private final ExecutorService downloaders;
     private final ExecutorService extractors;
+    private final int perHost;
 
     public WebCrawler(Downloader downloader, int downloaders, int extractors, int perHost) {
+        if (downloader == null || downloaders < 1 || extractors < 1 || perHost < 1) {
+            throw new IllegalArgumentException("One of given arguments is incorrect");
+        }
         this.downloader = downloader;
         this.downloaders = Executors.newFixedThreadPool(Integer.min(Runtime.getRuntime().availableProcessors(), downloaders));
         this.extractors = Executors.newFixedThreadPool(Integer.min(Runtime.getRuntime().availableProcessors(), extractors));
+        this.perHost = perHost;
+    }
+
+    private class HostTraffic {
+        private int cnt;
+        private final Queue<Runnable> extra;
+
+        private HostTraffic() {
+            cnt = 1;
+            extra = new LinkedList<>();
+        }
+
+        private synchronized void addTask(Runnable task) {
+            if (cnt < perHost) {
+                downloaders.submit(task);
+                cnt++;
+            } else {
+                extra.add(task);
+            }
+        }
+
+        private synchronized Runnable getTask() {
+            if (extra.isEmpty()) {
+                cnt--;
+            }
+            return extra.poll();
+        }
     }
 
     @Override
@@ -26,11 +62,20 @@ public class WebCrawler implements Crawler {
         if (url == null || depth < 1) {
             throw new IllegalArgumentException("One of given arguments is incorrect");
         }
+        String host;
+        try {
+            host = getHost(url);
+        } catch (MalformedURLException e) {
+            IllegalArgumentException exception = new IllegalArgumentException("Couldn't get host of given url");
+            exception.addSuppressed(e);
+            throw exception;
+        }
         List<String> downloaded = new CopyOnWriteArrayList<>();
         Map<String, IOException> errors = new ConcurrentHashMap<>();
-        ConcurrentSkipListSet<String> set = new ConcurrentSkipListSet<>(List.of(url));
+        ConcurrentSkipListSet<String> pages = new ConcurrentSkipListSet<>(List.of(url));
+        ConcurrentHashMap<String, HostTraffic> hosts = new ConcurrentHashMap<>(Map.of(host, new HostTraffic()));
         Phaser phaser = new Phaser(1);
-        downloaders.submit(new DownloadTask(downloaded, errors, depth, url, 1, set, phaser));
+        downloaders.submit(new DownloadTask(downloaded, errors, depth, url, 1, pages, hosts, phaser));
         phaser.arriveAndAwaitAdvance();
         return new Result(downloaded, errors);
     }
@@ -47,17 +92,21 @@ public class WebCrawler implements Crawler {
         private final int maxDepth;
         private final String url;
         private final int curDepth;
-        private final ConcurrentSkipListSet<String> set;
+        private final ConcurrentSkipListSet<String> pages;
+        private final ConcurrentHashMap<String, HostTraffic> hosts;
         private final Phaser phaser;
 
         private DownloadTask(List<String> downloaded, Map<String, IOException> errors, int maxDepth,
-                             String url, int curDepth, ConcurrentSkipListSet<String> set, Phaser phaser) {
+                             String url, int curDepth,
+                             ConcurrentSkipListSet<String> pages, ConcurrentHashMap<String, HostTraffic> hosts,
+                             Phaser phaser) {
             this.downloaded = downloaded;
             this.errors = errors;
             this.maxDepth= maxDepth;
             this.url = url;
             this.curDepth= curDepth;
-            this.set = set;
+            this.pages = pages;
+            this.hosts = hosts;
             this.phaser = phaser;
             phaser.register();
         }
@@ -68,12 +117,23 @@ public class WebCrawler implements Crawler {
                 Document document = downloader.download(url);
                 downloaded.add(url);
                 if (curDepth != maxDepth) {
-                    extractors.submit(new ExtractTask(downloaded, errors, maxDepth, document, url, curDepth, set, phaser));
+                    extractors.submit(new ExtractTask(downloaded, errors, maxDepth, document, url, curDepth, pages, hosts, phaser));
                 }
             } catch (IOException e) {
                 errors.put(url, e);
             } finally {
                 phaser.arrive();
+                String host = null; //this url is got from Document::extractLinks -> assume that's correct
+                try {
+                    host = getHost(url);
+                } catch (MalformedURLException impossible) {
+                    System.err.println("IMPOSSIBLE");
+                }
+                assert host != null;
+                Runnable downloadTask = hosts.get(host).getTask();
+                if (downloadTask != null) {
+                    downloaders.submit(downloadTask);
+                }
             }
         }
     }
@@ -85,18 +145,22 @@ public class WebCrawler implements Crawler {
         private final Document document;
         private final String url;
         private final int curDepth;
-        private final ConcurrentSkipListSet<String> set;
+        private final ConcurrentSkipListSet<String> pages;
+        private final ConcurrentHashMap<String, HostTraffic> hosts;
         private final Phaser phaser;
 
         private ExtractTask(List<String> downloaded, Map<String, IOException> errors, int maxDepth,
-                            Document document, String url, int curDepth, ConcurrentSkipListSet<String> set, Phaser phaser) {
+                            Document document, String url, int curDepth,
+                            ConcurrentSkipListSet<String> pages, ConcurrentHashMap<String, HostTraffic> hosts,
+                            Phaser phaser) {
             this.downloaded = downloaded;
             this.errors = errors;
             this.maxDepth = maxDepth;
             this.document = document;
             this.url = url;
             this.curDepth = curDepth;
-            this.set = set;
+            this.pages = pages;
+            this.hosts = hosts;
             this.phaser = phaser;
             phaser.register();
         }
@@ -105,8 +169,15 @@ public class WebCrawler implements Crawler {
         public void run() {
             try {
                 for (String s : document.extractLinks()) {
-                    if (set.add(s)) {
-                        downloaders.submit(new DownloadTask(downloaded, errors, maxDepth, s, curDepth + 1, set, phaser));
+                    if (pages.add(s)) {
+                        String host = getHost(s);
+                        Runnable downloadTask = new DownloadTask(downloaded, errors, maxDepth, s, curDepth + 1, pages, hosts, phaser);
+                        if (!hosts.containsKey(host)) {
+                            hosts.put(host, new HostTraffic());
+                            downloaders.submit(downloadTask);
+                        } else {
+                            hosts.get(host).addTask(downloadTask);
+                        }
                     }
                 }
             } catch (IOException e) {
